a7eee717c9cb2abf5eebe972f50f465410d9acdc
meowhard <work.aak@yandex.ru>
2022-11-17 14:08:34 +0300

    17.11

--- src/Main.java
+++ src/Main.java
@@ -1,118 +1,38 @@
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args){
         Calc calc = new Calc();
-//        human.getOverPayment();
-//        human.abc();
-//        calc.human();
-//        calc.business();
-//        calc.humanTest();
-        calc.businessTest();
-//        calc.hueta();
+        calc.getOverpayment();
     }
 }
+
 class Calc{
-    private double credit = 200000.0;
-    private double payment = 10000.0;
-    private double percent = 10.0;
+    Scanner input = new Scanner(System.in);
+    String inputText = input.nextLine();
+    String[] inputArray = inputText.split(" ");
+    private double credit = Double.parseDouble(inputArray[0]);
+    private double payment = Double.parseDouble(inputArray[1]);
+    private double percent = Double.parseDouble(inputArray[2]);
+    private String person = inputArray[3];
     private double newCredit = credit;
     private double totalPayment = 0;
     private double result;
 
-    public void hueta () {
+    public void getOverpayment(){
         int count = 0;
-        newCredit = credit * (percent / 100 + 1);
-        while (newCredit > totalPayment) {
-            count++;
-            totalPayment = totalPayment + payment;
-            if (totalPayment > newCredit) {
-                totalPayment = newCredit;
-                break;
-            }
-            if (count > 11) {
-                newCredit = (newCredit - totalPayment) * (percent / 100 + 1) + totalPayment;
-                count = 0;
 
-            }
-        }
-        System.out.println(newCredit);
-    }
-
-    public void human(){
-        int count = 0;
-        while(newCredit > payment){
-            newCredit = newCredit * (percent / 100 + 1);
-            count = 0;
-            while(newCredit > payment && count < 12) {
-                newCredit = newCredit - payment;
-                totalPayment = totalPayment + payment;
-                count++;
-            }
-        }
-        if (count == 12) {
-            newCredit = newCredit * (percent / 100 + 1);
-        }
-        totalPayment = totalPayment + newCredit - credit;
-        result = Math.round(totalPayment);
-        System.out.println(result);
-    }
-
-    public void business(){
-        int count;
-        while(newCredit > payment){
-            count = 0;
-            while(newCredit > payment && count < 12) {
-                newCredit = newCredit - payment;
-                totalPayment = totalPayment + payment;
-                count++;
-            }
-            if(newCredit>payment) {
+        switch (person){
+            case "human":
                 newCredit = newCredit * (percent / 100 + 1);
-            }
-        }
-        totalPayment = totalPayment + newCredit - credit;
-        result = Math.round(totalPayment);
-        System.out.println(result);
-    }
-
-    public void abc () {
-        int count;
-
-        while(newCredit > payment){
-            count = 0;
-            while(newCredit > payment && count < 12) {
-                newCredit = newCredit - payment;
-                totalPayment = totalPayment + payment;
-                count++;
-            }
-            newCredit = newCredit * (percent / 100 + 1);
+                break;
+            case "business":
+                break;
         }
 
 
-        totalPayment = totalPayment + newCredit - credit;
-        result = Math.ceil(totalPayment);
-        System.out.println(result);
-    }
-
-    public void humanTest() {
-        int count = 0;
-        newCredit = newCredit * (percent / 100 + 1);
-        while (totalPayment < newCredit){
-            totalPayment = totalPayment + payment;
-            count++;
-            if (totalPayment > newCredit){
-                totalPayment = newCredit;
-            }
-            if (count > 11){
-                newCredit = (newCredit - totalPayment) * (percent / 100 + 1) + totalPayment;
-                count = 0;
-            }
-        }
-        result = Math.round(newCredit - credit);
-        System.out.println(result);
-    }
-
-    public void businessTest() {
-        int count = 0;
         while (totalPayment < newCredit){
             totalPayment = totalPayment + payment;
             count++;
